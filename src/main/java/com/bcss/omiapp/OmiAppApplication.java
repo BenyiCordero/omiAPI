7c3a93d7d52da0b82fcc9a39f1f010e61129d872
BenyiCordero <benyi.uriel2006@gmail.com>
2025-10-31 09:54:17 -0600

    Creacion del proyecto

---
+++ src/main/java/com/bcss/omiapp/OmiAppApplication.java
@@ -0,0 +1,13 @@
+package com.bcss.omiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmiAppApplication.class, args);
+    }
+
+}
