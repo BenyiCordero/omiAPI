32b6fa0e633bd4f76b4bae8a3a3025e0b2ec456b
BenyiCordero <benyi.uriel2006@gmail.com>
2025-10-31 12:13:05 -0600

    Se crean los servicios para las personas

---
+++ src/main/java/com/bcss/omiapp/service/PersonaServiceImpl.java
@@ -0,0 +1,46 @@
+package com.bcss.omiapp.service;
+
+import com.bcss.omiapp.domain.Persona;
+import com.bcss.omiapp.exception.PersonaRepetidaException;
+import com.bcss.omiapp.exception.UsuarioNoEncontradoException;
+import com.bcss.omiapp.repository.PersonaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PersonaServiceImpl implements PersonaService {
+
+    private final PersonaRepository personaRepository;
+
+    public PersonaServiceImpl(PersonaRepository personaRepository) {
+        this.personaRepository = personaRepository;
+    }
+
+    @Override
+    public Persona save(Persona persona) {
+        if(personaRepository.existsPersonaByNumeroTelefono(persona.getNumeroTelefono())) throw new PersonaRepetidaException();
+        return personaRepository.save(persona);
+    }
+
+    @Override
+    public Optional<Persona> findByTelefono(String telefono) {
+        Optional<Persona> persona = personaRepository.findByTelefono(telefono);
+        if(persona.isPresent()) return persona;
+        else throw new UsuarioNoEncontradoException();
+    }
+
+    @Override
+    public Optional<Persona> findById(Integer id) {
+        Optional<Persona> persona = personaRepository.findById(id);
+        if(persona.isPresent()) return persona;
+        else throw new UsuarioNoEncontradoException();
+    }
+
+    @Override
+    public Optional<Persona> findByNombre(String nombre) {
+        Optional<Persona> persona = personaRepository.findByNombre(nombre);
+        if(persona.isPresent()) return persona;
+        else throw new UsuarioNoEncontradoException();
+    }
+}
