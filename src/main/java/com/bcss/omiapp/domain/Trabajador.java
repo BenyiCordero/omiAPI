13d0b13d83f6bd10b49f7a497423f61d3fedffff
BenyiCordero <benyi.uriel2006@gmail.com>
2025-10-31 12:47:06 -0600

    Se agrega la clase Trabajador junto con sus atributos

---
+++ src/main/java/com/bcss/omiapp/domain/Trabajador.java
@@ -0,0 +1,30 @@
+package com.bcss.omiapp.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Trabajador")
+public class Trabajador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idUsuario;
+    @Column(nullable = false, length = 50, unique = true)
+    private String email;
+    @Column(nullable = false, length = 50)
+    private String password;
+    @Column(nullable = false)
+    private Float horasSemana;
+    @Column(nullable = false)
+    private Float salario;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "persona_id")
+    private Persona persona;
+
+}
