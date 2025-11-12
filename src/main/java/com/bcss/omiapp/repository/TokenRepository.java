f80b5539e6bdf1a1bbd0a740261d68d4b3132190
BenyiCordero <benyi.uriel2006@gmail.com>
2025-11-12 09:55:42 -0600

    Se agrega el repositorio del token

---
+++ src/main/java/com/bcss/omiapp/repository/TokenRepository.java
@@ -0,0 +1,13 @@
+package com.bcss.omiapp.repository;
+
+import com.bcss.omiapp.domain.Token;
+import com.bcss.omiapp.domain.Trabajador;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token, Integer> {
+    Optional<Token> findByToken(String token);
+    List<Token> findByTrabajador(Trabajador trabajador);
+}
