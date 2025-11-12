824f04072b63938372f63ba0e19421c328feebcb
BenyiCordero <benyi.uriel2006@gmail.com>
2025-11-12 09:58:50 -0600

    Se agregan las excepciones de token invaldo y credenciales invalidas

---
+++ src/main/java/com/bcss/omiapp/exception/TokenInvalidoException.java
@@ -0,0 +1,7 @@
+package com.bcss.omiapp.exception;
+
+public class TokenInvalidoException extends RuntimeException {
+    public TokenInvalidoException() {
+        super("Token invalido");
+    }
+}
